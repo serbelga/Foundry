69867ebdddd7e7eb4f3340bfcfd3c531c139b925
Sergio Belda <sbeldagalbis@gmail.com>
2022-07-29 18:16:14 +0200

    Add spotless.gradle and run spotlessApply

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("com.android.application") version "7.4.0-alpha08" apply false
     id("com.android.library") version "7.4.0-alpha08" apply false
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
-    id("com.diffplug.spotless") version "6.9.0"
+    alias(libs.plugins.spotless)
 }
 
 subprojects {
+    apply("${rootDir}/spotless.gradle")
     apply {
         from("${rootDir}/ktlint.gradle.kts")
     }
